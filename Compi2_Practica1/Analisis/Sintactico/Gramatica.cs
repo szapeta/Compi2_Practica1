368e26c4763b9a1e496c720fecb6e52d445c89a4
sfelipe <sfelipe@bdgsa.net>
2021-01-31 23:13:05 -0600

    Add project files.

---
+++ Compi2_Practica1/Analisis/Sintactico/Gramatica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace Compi2_Practica1.Analisis.Sintactico
+{
+    class Gramatica
+    {
+    }
+}
